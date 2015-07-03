b88b6dd665993838639898280e9202abbe93facb
gojas <gojas90@gmail.com>
2015-07-03 15:07:11 +0200

    =questionnaire bundle added

---
+++ symfony/src/QuestionnaireBundle/Controller/DefaultController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace QuestionnaireBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction($name)
+    {
+        return $this->render('QuestionnaireBundle:Default:index.html.twig', array('name' => $name));
+    }
+}
