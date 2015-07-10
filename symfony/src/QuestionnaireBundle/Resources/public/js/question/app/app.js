03c79f5c119a5907be06b013a318130c46087e54
gojas <gojas90@gmail.com>
2015-07-10 12:53:44 +0200

    =dynamic routes

--- symfony/src/QuestionnaireBundle/Resources/public/js/question/app/app.js
+++ symfony/src/QuestionnaireBundle/Resources/public/js/question/app/app.js
@@ -8,30 +8,12 @@ window.Question = {
         var question = new Question.Models.Question(),
             router = new Question.Router();
 
+        router.on("route", function(route, params) {
+            view = route.charAt(0).toUpperCase() + route.slice(1);
 
-
-        router.on('route:home', function() {
-            var homeView = new Question.Views.Home();
+            var homeView = new Question.Views[view]();
 
             $('#question').html(homeView.render().$el);
-        });
-
-        router.on('route:question', function() {
-            var view = new Question.Views.Question();
-
-            $('#question').html(view.render().$el);
-        });
-
-        router.on('route:login', function() {
-            var view = new Question.Views.Login();
-
-            $('#question').html(view.render().$el);
-        });
-
-        router.on('route:registration', function() {
-            var view = new Question.Views.Registration();
-
-            $('#question').html(view.render().$el);
         });
 
         Backbone.history.start();
