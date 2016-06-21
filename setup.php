f26a7b3a4bfa1a1083d278bc1139be643ffd2f0a
Laslo Horvath <bosske1@gmail.com>
2016-06-21 02:56:59 +0200

    setup scripts

--- setup.php
+++ setup.php
@@ -1,1 +1,16 @@
 <?php
+
+//echo shell_exec('./setup.sh');
+
+//echo shell_exec('./setup.sh ');
+/*
+php app/console cache:clear --env=prod --no-debug
+php app/console assetic:dump --env=prod --no-debug
+*/
+
+$dir = getcwd();
+
+echo $dir;
+
+var_dump(shell_exec('/usr/bin/php ' . $dir . '/app/console cache:clear --env=prod --no-debug 2>&1'));
+var_dump(shell_exec('/usr/bin/php ' . $dir . '/app/console assetic:dump --env=prod --no-debug 2>&1'));
